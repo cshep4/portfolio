557aecd36706e54f2b39501c9ff3c1eb7cc7366a
cshep4 <chris_shepherd2@hotmail.com>
2018-08-29 23:51:03 +0100

    started conversion to angular 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {AppNavbarComponent} from './app-navbar/app-navbar.component';
+import {PreloaderComponent} from './preloader/preloader.component';
+import {TopHeaderComponent} from './top-header/top-header.component';
+import {LeftSidebarComponent} from './left-sidebar/left-sidebar.component';
 
 @NgModule({
   declarations: [
+    PreloaderComponent,
+    TopHeaderComponent,
+    LeftSidebarComponent,
     AppNavbarComponent,
     AppComponent
   ],
