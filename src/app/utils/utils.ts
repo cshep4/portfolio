74fbd280d745bb70e5e5c861dbebfe0071513795
cshep4 <chris_shepherd2@hotmail.com>
2018-10-09 20:32:01 +0100

    Updated links, styling changes

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -7,5 +7,5 @@ export interface RequestOptions {
 
 // export const apiUrl = 'http://localhost:1337/';
 // export const apiUrl = 'https://portfolio-be-api.herokuapp.com/';
-export const apiUrl = 'https://ukjxrax9hk.execute-api.us-east-1.amazonaws.com/Prod/';
+export const apiUrl = 'https://kcu4krtu92.execute-api.us-east-1.amazonaws.com/dev/';
 
